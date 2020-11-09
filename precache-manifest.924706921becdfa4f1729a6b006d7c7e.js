628a443520857ec4f461fd0a4d521832e5904ddf
Aminamary1 <48100143+Aminamary1@users.noreply.github.com>
2020-11-09 11:18:39 -0500

    Updates

---
+++ precache-manifest.924706921becdfa4f1729a6b006d7c7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b43f67d9a9599c548b427ee1560714",
+    "url": "/syllabus-app/index.html"
+  },
+  {
+    "revision": "17109115c496c3f49f27",
+    "url": "/syllabus-app/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "899b0e315335ae89c611",
+    "url": "/syllabus-app/static/css/main.16d7ee3b.chunk.css"
+  },
+  {
+    "revision": "17109115c496c3f49f27",
+    "url": "/syllabus-app/static/js/2.8a6f6bff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/syllabus-app/static/js/2.8a6f6bff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "899b0e315335ae89c611",
+    "url": "/syllabus-app/static/js/main.b71b4810.chunk.js"
+  },
+  {
+    "revision": "c867941a036455bb2301",
+    "url": "/syllabus-app/static/js/runtime-main.169eb2a4.js"
+  }
+]);
